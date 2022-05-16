de9c27216a2df7f46b36a5e237492035b57fd2bb
Irek Iwanca <iiwanca@gmail.com>
2022-05-16 11:16:27 +0200

    Added AdRecord.

---
+++ types/ad/ad-entity.ts
@@ -0,0 +1,9 @@
+export interface AdEntity {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    url: string;
+    lat: number;/*latitude - szerokość geograficzna*/
+    lon: number;/*longitude - długość geograficzna*/
+}
